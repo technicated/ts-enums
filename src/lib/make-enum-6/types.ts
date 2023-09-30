92a8532e6e7cf690c3c6b28a9298c242169681f4
Andrea Tomarelli <andrea.tomarelli@weedea.com>
2023-09-30 22:04:36 +0200

    Refactored make-enum-6

--- src/lib/make-enum-6/types.ts
+++ src/lib/make-enum-6/types.ts
@@ -1,70 +1,35 @@
-import { cases } from '../case'
+import { cases, Cast } from '../case'
 import { HKT6, Kind6 } from '../hkt'
-import * as types from '../make-enum-0/types'
-import { Incr } from '../type-arithmetic'
+import { Unit } from '../unit'
 
-export type EnumShape = HKT6 & { type: types.EnumShape }
-export type ProtoShape = HKT6 & { type: types.ProtoShape }
+export type EnumShape = HKT6 & { readonly type: { readonly case: string } }
 
-type RecursiveCtorArgs<
-  Obj extends object,
-  Index extends number = 0
-> = Obj extends Record<Index, infer V>
-  ? [V, ...RecursiveCtorArgs<Omit<Obj, Index>, Incr<Index>>]
-  : []
+type CasesOfEnum<EnumHKT extends EnumShape> = {
+  [Case in EnumHKT['type']['case']]: Case
+}
 
 type EnumCtorArgs<
-  Enum extends EnumShape,
-  Case extends Enum['type']['case'],
-  Proto extends ProtoShape,
+  EnumHKT extends EnumShape,
+  Case extends EnumHKT['type']['case'],
   A,
   B,
   C,
   D,
   E,
   F
-> = Kind6<Enum, A, B, C, D, E, F> & {
-  _: unknown
-  case: Case
-} extends infer _T
-  ? keyof Omit<
-      _T,
-      'case' | keyof Kind6<Proto, A, B, C, D, E, F>
-    > extends keyof {
-      _: unknown
-    }
+> = Cast<Kind6<EnumHKT, A, B, C, D, E, F>, Case> extends { p: infer Payload }
+  ? Payload extends Unit
     ? []
-    : 0 extends keyof Omit<
-        _T,
-        'case' | '_' | keyof Kind6<Proto, A, B, C, D, E, F>
-      >
-    ? [RecursiveCtorArgs<Omit<_T, 'case'>>]
-    : Record<string, never> extends Omit<
-        _T,
-        'case' | '_' | keyof Kind6<Proto, A, B, C, D, E, F>
-      >
-    ? Partial<[Omit<_T, 'case' | '_' | keyof Kind6<Proto, A, B, C, D, E, F>>]>
-    : [Omit<_T, 'case' | '_' | keyof Kind6<Proto, A, B, C, D, E, F>>]
+    : [payload: Payload]
   : never
 
-type CasesOfEnum<Enum extends EnumShape> = {
-  [Case in Enum['type']['case']]: Case
-}
+export type EnumCtors<EnumHKT extends EnumShape> = {
+  [Case in EnumHKT['type']['case']]: <A, B, C, D, E, F>(
+    ...args: EnumCtorArgs<EnumHKT, Case, A, B, C, D, E, F>
+  ) => Kind6<EnumHKT, A, B, C, D, E, F>
+} & Record<typeof cases, CasesOfEnum<EnumHKT>>
 
-export type CasesOf<EnumType> = EnumType extends EnumCtors<
-  infer Enum,
-  ProtoShape
->
-  ? keyof CasesOfEnum<Enum>
-  : never
-
-export type EnumCtors<Enum extends EnumShape, Proto extends ProtoShape> = {
-  [Case in Enum['type']['case']]: <A, B, C, D, E, F>(
-    ...args: EnumCtorArgs<Enum, Case, Proto, A, B, C, D, E, F>
-  ) => Kind6<Enum, A, B, C, D, E, F>
-} & Record<typeof cases, CasesOfEnum<Enum>>
-
-export type MakeProtoFn<Enum extends EnumShape, Proto extends ProtoShape> = <
+type MakeProtoFn<EnumHKT extends EnumShape, EnumType extends object> = <
   A,
   B,
   C,
@@ -72,5 +37,27 @@ export type MakeProtoFn<Enum extends EnumShape, Proto extends ProtoShape> = <
   E,
   F
 >(
-  e: EnumCtors<Enum, Proto>
-) => Kind6<Proto, A, B, C, D, E, F> & ThisType<Kind6<Enum, A, B, C, D, E, F>>
+  Enum: [EnumType] extends [never]
+    ? EnumCtors<EnumHKT>
+    : EnumCtors<EnumHKT> & EnumType
+) => ThisType<Kind6<EnumHKT, A, B, C, D, E, F>> &
+  Omit<Kind6<EnumHKT, A, B, C, D, E, F>, 'case' | 'p'>
+
+export type MakeEnumFnArgs<
+  EnumHKT extends EnumShape,
+  EnumType extends object = never
+> = [EnumType] extends [never]
+  ? EnumHKT['type'] & { _: unknown } extends infer _T
+    ? keyof Omit<_T, 'case' | 'p'> extends '_'
+      ? []
+      : [{ makeProto: MakeProtoFn<EnumHKT, EnumType> }]
+    : never
+  : EnumHKT['type'] & { _: unknown } extends infer _T
+  ? keyof Omit<_T, 'case' | 'p'> extends '_'
+    ? [{ type: EnumType }]
+    : [{ makeProto: MakeProtoFn<EnumHKT, EnumType>; type: EnumType }]
+  : never
+
+export type CasesOf<Ctors> = Ctors extends EnumCtors<infer EnumHKT>
+  ? EnumHKT['type']['case']
+  : never
