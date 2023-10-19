6d74d8d94cdb1a1591b3a339872bdb7ec4db7ce3
technicated <andrea.tomarelli+technicated@gmail.com>
2023-10-19 17:27:25 +0200

    Documentation

--- src/lib/case.ts
+++ src/lib/case.ts
@@ -1,5 +1,33 @@
 import { Unit } from './unit'
 
+/**
+ * An interface defining the basic shape of an enum instance, consisting of the
+ * properties `case` and `p`.
+ *
+ * Use this type to constrain generic types or functions to accept instances of
+ * enums, like in the following example:
+ *
+ * ```typescript
+ * function performIfMatch<E extends EnumShape, C extends E['case']>(
+ *   enumValue: E,
+ *   enumCase: C,
+ *   operation: (value: Cast<E, C>['p']) => void
+ * ): boolean { ... }
+ *
+ * type NumberOrString = Case<'number', number> | Case<'string', string>
+ * const NumberOrString = makeEnum<NumberOrString>()
+ *
+ * // will return `true`
+ * performIfMatch(NumberOrString.number(42), 'number', (val) => {
+ *   // val is of type `number`
+ * })
+ *
+ * // will return `false`
+ * performIfMatch(NumberOrString.number(42), 'string', (val) => {
+ *   // val is of type `string`
+ * })
+ * ```
+ */
 export interface EnumShape {
   readonly case: string
   readonly p: unknown
